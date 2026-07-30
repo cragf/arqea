5d490a486fcbf639b5eac7672da2abbb8aa1e7f2
cragf <carlosrobertoag0416@gmail.com>
2026-07-30 19:51:25 -0400

    initial commit

---
+++ arqea/src/main/java/com/robert/arqea/ArqeaApplication.java
@@ -0,0 +1,13 @@
+package com.robert.arqea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqeaApplication.class, args);
+	}
+
+}
---
+++ arqea/src/test/java/com/robert/arqea/ArqeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robert.arqea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
